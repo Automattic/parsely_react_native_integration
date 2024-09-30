4b132a5f4ef66567bff6dcbe7e0435ea91d6b7a2
Gio Lodi <gio@mokacoding.com>
2024-09-30 14:01:23 +1000

    Wire `ParselyNativeModule` with Parsely Android SDK

--- android/app/src/main/java/com/parsely/react_native_example/ParselyNativeModule.kt
+++ android/app/src/main/java/com/parsely/react_native_example/ParselyNativeModule.kt
@@ -5,6 +5,8 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 
+import com.parsely.parselyandroid.ParselyTracker;
+
 class ParselyNativeModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
     // Note: The name and all the method signatures must be the same as the Swift implementation.
@@ -13,21 +15,38 @@ class ParselyNativeModule(reactContext: ReactApplicationContext) : ReactContextB
 
     @ReactMethod
     fun configureWithSiteId(siteId: String) {
-        Log.d("ParselyNativeModule", "Stub for configuration method. Input: $siteId")
+        val context = this.reactApplicationContext
+        val activity = currentActivity
+
+        if (activity == null) {
+            Log.e("ParselyNativeModule", "Unable to initialize ParselyTracker: currentActivity is null!")
+            // TODO: Should we fail or throw an error here to avoid giving the user the impression the SDK started successfully?
+            return
+        }
+
+        activity.runOnUiThread {
+            ParselyTracker.init(siteId, 30, this.reactApplicationContext, false)
+            Log.d("ParselyNativeModule", "ParselyTracker configured with: $siteId")
+        }
     }
 
     @ReactMethod
     fun startEngagementWithURL(urlString: String) {
-        Log.d("ParselyNativeModule", "Stub for start engagement method. Input: $urlString")
+        // "Engagement session cannot start without calling trackPageview first"
+        trackPageViewWithURL(urlString)
+        ParselyTracker.sharedInstance().startEngagement(urlString)
+        Log.d("ParselyNativeModule", "Engagement started with: $urlString")
     }
 
     @ReactMethod
     fun trackPageViewWithURL(urlString: String) {
-        Log.d("ParselyNativeModule", "Stub for track page view method. Input: $urlString")
+        ParselyTracker.sharedInstance().trackPageview(urlString)
+        Log.d("ParselyNativeModule", "Page view tracked with: $urlString")
     }
 
     @ReactMethod
     fun stopEngagement() {
-        Log.d("ParselyNativeModule", "Stub for stop method")
+        ParselyTracker.sharedInstance().stopEngagement()
+        Log.d("ParselyNativeModule", "Engagement stopped")
     }
 }
