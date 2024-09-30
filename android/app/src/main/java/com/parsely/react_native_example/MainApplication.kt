2247e2ea7b5dcd95f3e1b688fb7b3ebb38c8b60e
Gio Lodi <gio@mokacoding.com>
2024-09-30 13:30:50 +1000

    Add missing import to Android host — How come it built anyway?!

--- android/app/src/main/java/com/parsely/react_native_example/MainApplication.kt
+++ android/app/src/main/java/com/parsely/react_native_example/MainApplication.kt
@@ -15,6 +15,8 @@ import com.facebook.soloader.SoLoader
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
+import com.parsely.react_native_example.AppPackage
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
